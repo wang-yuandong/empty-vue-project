5edc830f6d756491cb90e301faeeb5ecb28e5254
wangyuandong <wangyd@tcpjw.com>
2023-11-10 16:17:17 +0800

    chore: init

--- lint-staged.config.cjs
+++ lint-staged.config.cjs
@@ -1,6 +1,8 @@
+/* eslint-env node */
 module.exports = {
-    'src/**/*.{js,jsx,ts,tsx,vue}': [
-        'prettier --write',
-        'eslint --fix',
-    ],
+  "src/**/*.{js,jsx,ts,tsx,vue}": [
+    "prettier --write",
+    "eslint --fix",
+    () => "vue-tsc --noEmit -p tsconfig.json --composite false --skipLibCheck"
+  ]
 };
