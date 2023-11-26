c3262f37eaaea76171ecb3db3540bc547b7f961c
wangyuandong <wangyd@tcpjw.com>
2023-11-26 14:21:38 +0800

    feat: init h5 project

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,5 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+<script setup></script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <main>1231231</main>
 </template>
